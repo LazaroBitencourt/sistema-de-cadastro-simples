e8216ebdac07c674da45458da8b960ffaae4a177
[name] <[email address]>
2024-03-01 23:34:31 -0300

    cria query para buscar usuario pelo nome

--- src/main/java/com/github/LazaroBitencourt/sistemadecadastrosimples/repository/UsuarioRepository.java
+++ src/main/java/com/github/LazaroBitencourt/sistemadecadastrosimples/repository/UsuarioRepository.java
@@ -2,8 +2,13 @@ package com.github.LazaroBitencourt.sistemadecadastrosimples.repository;
 
 import com.github.LazaroBitencourt.sistemadecadastrosimples.model.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario,Long> {
+    @Query("SELECT u FROM Usuario u WHERE upper(trim(u.nome)) LIKE %?1%")
+    List<Usuario> buscaPeloNome(String nome);
 }
