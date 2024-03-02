239abcbcaf8fc9c5a2766b2d399183c3b6602b69
[name] <[email address]>
2024-03-02 19:33:47 -0300

    implementa configuracao para resolver problemas de CORS

--- src/main/java/com/github/LazaroBitencourt/sistemadecadastrosimples/config/WebConfig.java
+++ src/main/java/com/github/LazaroBitencourt/sistemadecadastrosimples/config/WebConfig.java
@@ -9,6 +9,9 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
-    registry.addMapping("/**");
+    registry.addMapping("/**")
+            .allowedMethods("GET", "POST", "PUT", "DELETE");
+
+
     }
 }
