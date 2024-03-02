931cc6c3bf5ec91d48a0cb3b50e9a255fe846ab2
[name] <[email address]>
2024-03-01 23:35:14 -0300

    finaliza crud de usuario

--- src/main/java/com/github/LazaroBitencourt/sistemadecadastrosimples/controller/UsuarioController.java
+++ src/main/java/com/github/LazaroBitencourt/sistemadecadastrosimples/controller/UsuarioController.java
@@ -5,12 +5,10 @@ import com.github.LazaroBitencourt.sistemadecadastrosimples.repository.UsuarioRe
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Locale;
 
 @RestController
 @RequestMapping("/usuarios")
@@ -25,4 +23,43 @@ public class UsuarioController {
         List<Usuario> usuarios = usuarioRepository.findAll();
         return new ResponseEntity<List<Usuario>>(usuarios,HttpStatus.OK);
     }
+
+    @PostMapping("/salvar")
+    @ResponseBody
+    public ResponseEntity<Usuario> salvar(@RequestBody Usuario usuario){
+        Usuario novoUsuario = usuarioRepository.save(usuario);
+        return new ResponseEntity<Usuario>(novoUsuario,HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("/deletar-usuario/{id}")
+    @ResponseBody
+    public ResponseEntity<String> deletar (@PathVariable("id") Long id){
+     usuarioRepository.deleteById(id);
+     return new ResponseEntity<String>("Usuário deletado com sucesso", HttpStatus.OK);
+    }
+
+    @GetMapping("/buscar-usuario/{id}")
+    @ResponseBody
+    public ResponseEntity<Usuario> buscarUsuariopeloId (@PathVariable("id") Long id){
+     Usuario usuario = usuarioRepository.findById(id).get();
+     return new ResponseEntity<Usuario>(usuario,HttpStatus.OK);
+    }
+
+    @PutMapping("/atualizar-usurio")
+    @ResponseBody
+    public ResponseEntity<?> atualizar(@RequestBody Usuario usuario) {
+        if(usuario.getId() == null){
+            return new ResponseEntity<String>("Id não foi informado para atualização", HttpStatus.BAD_REQUEST);
+        }
+        Usuario usuarioAtualizado = usuarioRepository.saveAndFlush(usuario);
+        return new ResponseEntity<Usuario>(usuarioAtualizado, HttpStatus.OK);
+    }
+
+    @GetMapping("/busca-pelo-nome/{nome}")
+    @ResponseBody
+    public ResponseEntity<List<Usuario>> buscarUsuarioPeloNome (@PathVariable("nome") String nome){
+        List<Usuario> usuarios = usuarioRepository.buscaPeloNome(nome.trim().toUpperCase());
+        return new ResponseEntity<List<Usuario>>(usuarios,HttpStatus.OK);
+    }
+
 }
